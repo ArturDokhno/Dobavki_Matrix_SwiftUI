fc3d0b43e429ae528a56a1bff5263322d31c98bf
ArturDokhno <artur.dohno@icloud.com>
2022-02-06 10:58:38 +0500

    Initial Commit

---
+++ Dobavki_Matrix_SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Dobavki_Matrix_SwiftUI
+//
+//  Created by Артур Дохно on 06.02.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Dobavki_Matrix_SwiftUI/Dobavki_Matrix_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dobavki_Matrix_SwiftUIApp.swift
+//  Dobavki_Matrix_SwiftUI
+//
+//  Created by Артур Дохно on 06.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct Dobavki_Matrix_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
